4bea8d3b4e9e384e982da9428c357c795d361d3f
Mat Gilbert <matgilbert@gmail.com>
2019-01-15 19:48:50 -0500

    Working excel output of machine data

--- server/models/machine.js
+++ server/models/machine.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var Excel = require("exceljs");
+var unstream = require('unstream');
 
 module.exports = function(Machine) {
 
     Machine.ingest = function(req, res, cb){
 
         console.log(req.files);
-        cb(null, 'file uploaded');
+
         //1. Read entire CSV file
         //2. Insert one record per row
 
@@ -23,18 +24,11 @@ module.exports = function(Machine) {
             console.log('error',err);
             console.log('record', record);
 
-        })
+        });
+
+        cb(null, 'file uploaded');        
 
     }
-
-    Machine.export = function(payload, callback){
-
-        callback(null, payload);
-        //1. Query for all rows of Machine table
-        //2. iterate over all items and create array
-        //3. Use exceljs to respond with .xlxs file
-
-    }    
 
     //For importing CSV files
     Machine.remoteMethod('ingest', {
@@ -53,9 +47,62 @@ module.exports = function(Machine) {
 
     });
 
+
+    Machine.export = function(res,cb){
+
+        
+        //2. iterate over all items and create array
+        //3. Use exceljs to respond with .xlxs file
+
+        //1. Query for all rows of Machine table
+        Machine.find(null, (err, machines) => {
+
+
+
+            var workbook = new Excel.Workbook();
+            
+            var worksheet = workbook.addWorksheet('Machine Data');
+            
+            worksheet.columns = [
+                { header: 'ID', key: 'id' },
+                { header: 'Active', key: 'active' },
+                { header: 'Position', key: 'position' },
+                { header: 'Closing Time', key: 'closingTime' }
+            ];
+
+            worksheet.addRows(machines);
+
+            var datetime = new Date();
+            res.set('Expires', 'Tue, 03 Jul 2001 06:00:00 GMT');
+            res.set('Cache-Control', 'max-age=0, no-cache, must-revalidate, proxy-revalidate');
+            res.set('Last-Modified', datetime +'GMT');
+            res.set('Content-Type','application/force-download');
+            res.set('Content-Type','application/octet-stream');
+            res.set('Content-Type','application/download');
+            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            res.set('Content-Disposition','attachment;filename=machines.xlsx');
+            res.set('Content-Transfer-Encoding','binary');
+            
+            workbook.xlsx.write(unstream({}, function(data) {
+
+                res.send(data);
+                res.end();
+
+            }));
+            
+        });
+
+    }    
+
     Machine.remoteMethod('export', {
 
-        accepts: { arg: 'msg', type: 'string' }, 
+        http: {
+            verb: 'get',
+            status: 200
+        },
+        accepts: [
+            {arg: 'res', type: 'object', 'http': { source: 'res'}}
+        ],
         returns: { arg: 'response', type: 'string' },
         description: "Outputs an .xlxs file of all current machine data",
     
