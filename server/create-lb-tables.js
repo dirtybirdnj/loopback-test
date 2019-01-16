a2fc2cc0741438e270cd7ceb1eb5b3fe7aaf4e92
Mat Gilbert <matgilbert@gmail.com>
2019-01-15 23:59:24 -0500

    Testing end to end, making sure migration works properly from a fresh / emptyt DB

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,7 +1,7 @@
 var server = require('./server');
 var ds = server.dataSources.postgres;
-//var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Machine'];
-var lbTables = ['Machine']
+
+var lbTables = ['Machine', 'ACL'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
